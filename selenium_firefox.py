b929f607ec8f47ca7d6cbe52f0522641fb5fb905
wajmdrd <jilinwei1@gmail.com>
2025-02-07 19:18:19 +0100

    2nd commit

--- selenium_firefox.py
+++ selenium_firefox.py
@@ -4,6 +4,6 @@ from selenium.webdriver.firefox.service import Service
 service = Service(executable_path=r'C:\Webdriver\geckodriver.exe')
 driver = webdriver.Firefox(service=service)
 
-
+print('打开浏览器')
 driver.get('https://github.com')
 # 在此处添加其他操作（如查找元素、点击按钮等）
